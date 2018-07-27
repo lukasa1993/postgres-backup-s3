4dad01de32dd2480b7ec00ec950749d7f9c29065
luka dodelia <luka@picktek.com>
2018-07-27 10:24:57 +0400

    brief reformat

--- backup.sh
+++ backup.sh
@@ -50,19 +50,21 @@ else
 fi
 
 # env vars needed for aws tools
-export AWS_ACCESS_KEY_ID=$S3_ACCESS_KEY_ID
-export AWS_SECRET_ACCESS_KEY=$S3_SECRET_ACCESS_KEY
-export AWS_DEFAULT_REGION=$S3_REGION
+export AWS_ACCESS_KEY_ID=${S3_ACCESS_KEY_ID}
+export AWS_SECRET_ACCESS_KEY=${S3_SECRET_ACCESS_KEY}
+export AWS_DEFAULT_REGION=${S3_REGION}
 
-export PGPASSWORD=$POSTGRES_PASSWORD
+export PGPASSWORD=${POSTGRES_PASSWORD}
 POSTGRES_HOST_OPTS="-h $POSTGRES_HOST -p $POSTGRES_PORT -U $POSTGRES_USER $POSTGRES_EXTRA_OPTS"
+DUMP_START_TIME=$(date +"%Y%m%d%H%M%S")
+DUMP_FILE="/tmp/dump.sql.gz"
 
 echo "Creating dump of ${POSTGRES_DATABASE} database from ${POSTGRES_HOST}..."
 
-pg_dump $POSTGRES_HOST_OPTS $POSTGRES_DATABASE | gzip > dump.sql.gz
+pg_dump ${POSTGRES_HOST_OPTS} $POSTGRES_DATABASE | gzip > ${DUMP_FILE}
 
 echo "Uploading dump to $S3_BUCKET"
 
-cat dump.sql.gz | aws $AWS_ARGS s3 cp - s3://$S3_BUCKET/$S3_PREFIX/${POSTGRES_DATABASE}_$(date +"%Y-%m-%dT%H:%M:%SZ").sql.gz || exit 2
+cat ${DUMP_FILE} | aws ${AWS_ARGS} s3 cp - s3://${S3_BUCKET}/${S3_PREFIX}/${POSTGRES_DATABASE}_${DUMP_START_TIME}.sql.gz || exit 2
 
 echo "SQL backup uploaded successfully"
--- install.sh
+++ install.sh
@@ -16,7 +16,7 @@ apk del py2-pip
 
 # install go-cron
 apk add curl
-curl -L --insecure https://github.com/odise/go-cron/releases/download/v0.0.6/go-cron-linux.gz | zcat > /usr/local/bin/go-cron
+curl -L --insecure https://github.com/lukasa1993/go-cron/archive/v0.0.7.tar.gz | zcat > /usr/local/bin/go-cron
 chmod u+x /usr/local/bin/go-cron
 apk del curl
 
