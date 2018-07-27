c007a7d0e356a8013309f04781d6cc0f81bc43cd
luka dodelia <luka@picktek.com>
2018-07-27 10:48:04 +0400

    go-cron install

--- install.sh
+++ install.sh
@@ -15,10 +15,10 @@ pip install awscli
 apk del py2-pip
 
 # install go-cron
-apk add curl
-curl -L --insecure https://github.com/lukasa1993/go-cron/archive/v0.0.6.tar.gz | zcat > /usr/local/bin/go-cron
+wget https://github.com/lukasa1993/go-cron/releases/download/v0.0.7/go-cron-linux.gz
+gunzip go-cron-linux.gz
+mv ./go-cron-linux /usr/local/bin/go-cron
 chmod u+x /usr/local/bin/go-cron
-apk del curl
 
 
 # cleanup
